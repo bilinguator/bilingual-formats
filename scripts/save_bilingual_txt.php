0ee1dc1af665f2d7e3e9e75c6aa2cd464ccacaa3
Bilinguator <123996746+bilinguator@users.noreply.github.com>
2023-02-05 22:56:42 +0300

    Refactor save_bilingual_txt.php
    Add regular expressions, throw more exceptions, detect additional ASCII encoding.

--- scripts/save_bilingual_txt.php
+++ scripts/save_bilingual_txt.php
@@ -1,54 +1,46 @@
 <?php
 
-function saveBilingualTxt ($fileAddress1, $fileAddress2, $outputFileAddress = './bilingual.txt') {
+function saveBilingualTxt ($fileAddress1, $fileAddress2,
+    $outputFileAddress = '.' . DIRECTORY_SEPARATOR . 'bilingual.txt') {
 
     if (file_exists($fileAddress1) && file_exists($fileAddress2)) {
+        $outputDirectory = explode(DIRECTORY_SEPARATOR, $outputFileAddress);
+        $outputDirectory = array_slice($outputDirectory, 0, count($outputDirectory) - 1);
+        $outputDirectory = implode(DIRECTORY_SEPARATOR, $outputDirectory);
+        
+        if (!is_dir($outputDirectory)) {
+            throw new Exception("Directory $outputDirectory not found!");
+        }
+
         $fileContent1 = file_get_contents($fileAddress1);
         $fileContent2 = file_get_contents($fileAddress2);
 
-        $fileCoding1 = mb_detect_encoding($fileContent1, array('utf-8', 'cp1251'));
-        $fileCoding2 = mb_detect_encoding($fileContent2, array('utf-8', 'cp1251'));
+        $fileCoding1 = mb_detect_encoding($fileContent1, ['UTF-8', 'ASCII', 'CP1251']);
+        $fileCoding2 = mb_detect_encoding($fileContent2, ['UTF-8', 'ASCII', 'CP1251']);
 
         $textArray1 = preg_split("/[\r]*[\n]+/", iconv($fileCoding1, 'UTF-8', $fileContent1));
         $textArray2 = preg_split("/[\r]*[\n]+/", iconv($fileCoding2, 'UTF-8', $fileContent2));
 
-        foreach ([$textArray1, $textArray2] as $textArray) {
-            foreach ($textArray as $key => $value) {
-                foreach (['<h1>', '</h1>', '<b>', '</b>', '<i>', '</i>'] as $tag) {
-                    $value = implode('', explode($tag, $value));
-                }
-                $value = implode('\n', explode('<delimiter>', $value));
-            }
-        }
-
         $maxLength = max(count($textArray1), count($textArray2));
         $txtContent = '';
-        $tagsArray = ['<h1>', '</h1>', '<b>', '</b>', '<i>', '</i>'];
 
         for ($i = 0; $i < $maxLength; $i++) {
-            if (strpos($textArray1[$i], '<img') === false) {
-                foreach ($tagsArray as $tag) {
-                    $textArray1[$i] = implode('', explode($tag, $textArray1[$i]));
-                }
-                $textArray1[$i] = implode(PHP_EOL, explode('<delimiter>', $textArray1[$i]));
-                $txtContent .= trim($textArray1[$i]) . PHP_EOL . PHP_EOL;
-            }
-            if (strpos($textArray2[$i], '<img') === false) {
-                foreach (['<h1>', '</h1>', '<b>', '</b>', '<i>', '</i>'] as $tag) {
-                    $textArray2[$i] = implode('', explode($tag, $textArray2[$i]));
-                }
-                $textArray2[$i] = implode(PHP_EOL, explode('<delimiter>', $textArray2[$i]));
-                $txtContent .= trim($textArray2[$i]) . PHP_EOL . PHP_EOL;
-            }
+            $txtContent .= trim(@$textArray1[$i]) . PHP_EOL . PHP_EOL;
+            $txtContent .= trim(@$textArray2[$i]) . PHP_EOL . PHP_EOL;
         }
+
+        $txtContent = preg_replace("/<\/?(h1|b|i|img[0-9]+)>/i", '', $txtContent);
+        $txtContent = str_replace('<delimiter>', PHP_EOL, $txtContent);
+        $txtContent = preg_replace("/" . PHP_EOL . "{4,6}/", str_repeat(PHP_EOL, 2), $txtContent);
 
         file_put_contents($outputFileAddress, trim($txtContent));
     } else {
-        if (!file_exists($fileAddress1)) {
-            throw new Exception("File {$fileAddress1} not found.");
-        }
-        if (!file_exists($fileAddress2)) {
-            throw new Exception("File {$fileAddress2} not found.");
+        if (!file_exists($fileAddress1) && !file_exists($fileAddress2)) {
+            throw new Exception("Files $fileAddress1 and $fileAddress2 not found!");
+        } else if (!file_exists($fileAddress1)) {
+            throw new Exception("File $fileAddress1 not found!");
+        } else {
+            throw new Exception("File $fileAddress2 not found!");
         }
     }
 }
